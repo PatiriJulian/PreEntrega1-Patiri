a6758e79b6eb5557028dbe6b1e0560ee03134088
Patiri Julian <patirijulian94@gmail.com>
2023-01-28 10:50:11 -0300

    Creación del NavBar con bootsrap

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,7 +2,30 @@ function NavBar () {
 
   return(
     <header>
-    <h1>Aca va el header</h1>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Calzados</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Hombre</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Mujer</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Niños</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Niñas</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </header>
   )
 }
